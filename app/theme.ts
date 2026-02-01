eaef2c74b65b7a09d2908681d280a2f34f0d5ad3
Catalin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2026-02-01 21:30:32 +0200

    add some theme variables

--- app/theme.ts
+++ app/theme.ts
@@ -2,8 +2,35 @@
 import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
-  cssVariables: true,
+  cssVariables: { colorSchemeSelector: 'class' },
   typography: { fontFamily: 'var(--font-geist)' },
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: {
+          main: '#1ED760',
+        },
+      },
+    },
+    dark: {
+      palette: {
+        primary: {
+          main: '#1ED760',
+        },
+        background: {
+          default: '#121212',
+          paper: '#212121',
+        },
+      },
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: { textTransform: 'none' },
+      },
+    },
+  },
 });
 
 export default theme;
