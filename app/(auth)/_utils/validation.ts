c085324726696685b2de1432026088d29a40019c
Catalin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2026-02-08 15:15:42 +0200

    remove redundant file

--- app/(auth)/_utils/validation.ts
+++
@@ -1,8 +0,0 @@
-import z from 'zod';
-
-export const passwordSchema = z
-  .string()
-  .min(1, 'Password is required')
-  .min(6, 'Password must be at least 6 characters long')
-  .max(100, 'Password must be at most 100 characters long')
-  .refine(val => !/\s/.test(val), 'Password must not contain spaces');
