5d394baee5dd35914baf6604cceb14e2c94394e8
Catalin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2026-02-15 20:20:13 +0200

    position fab btn

--- components/Fab.tsx
+++ components/Fab.tsx
@@ -1,14 +1,19 @@
+'use client';
+
 import MuiFab from '@mui/material/Fab';
 import AddIcon from '@mui/icons-material/Add';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 export default function Fab() {
+  const notMobile = useMediaQuery('(min-width: 1000px)');
+
   return (
     <MuiFab
       color="primary"
       aria-label="add"
       sx={{
         position: 'absolute',
-        bottom: 102,
+        bottom: notMobile ? 100 : 81,
         right: 10,
       }}
     >
