f424b263579f81575cb50c71aea21857ff97877b
Catalin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2026-03-13 18:51:40 +0200

    manage categories button

---
+++ app/(with-navbar)/settings/@preferences/ManageCategoriesBtn.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import Button from '@mui/material/Button';
+import { useRouter } from 'next/navigation';
+import Category from '@mui/icons-material/Category';
+
+export default function ManageCategoriesBtn() {
+  const router = useRouter();
+
+  return (
+    <Button
+      variant="outlined"
+      startIcon={<Category />}
+      onClick={() => router.push('/categories')}
+    >
+      Manage Expense and Income Categories
+    </Button>
+  );
+}
--- app/(with-navbar)/settings/@preferences/page.tsx
+++ app/(with-navbar)/settings/@preferences/page.tsx
@@ -2,17 +2,22 @@ import { currencies } from '@/data/currency';
 import Section from '../_components/Section';
 import CurrencyAutocomplete from './CurrencyAutocomplete';
 import { requireAuth } from '@/lib/auth-utils';
+import Grid from '@mui/material/Grid';
+import ManageCategoriesBtn from './ManageCategoriesBtn';
 
 export default async function PreferencesPage() {
   const { user } = await requireAuth();
 
   return (
     <Section title="Preferences">
-      <CurrencyAutocomplete
-        key={user.id}
-        defaultValue={currencies.find(c => c.code === user?.currency)}
-        options={currencies.map(({ code, currency }) => ({ code, currency }))}
-      />
+      <Grid container spacing={3} direction="column">
+        <CurrencyAutocomplete
+          key={user.id}
+          defaultValue={currencies.find(c => c.code === user?.currency)}
+          options={currencies.map(({ code, currency }) => ({ code, currency }))}
+        />
+        <ManageCategoriesBtn />
+      </Grid>
     </Section>
   );
 }
