1240389886a4e9994de2907f73d0e9bb61f78e3f
Catalin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2026-03-25 19:12:09 +0200

    create expense DAL

--- data/expense.ts
+++ data/expense.ts
@@ -1,8 +1,40 @@
 import 'server-only';
 
-import { ExpenseFormValues } from '@/types/expense';
+import { sql } from '@/lib/neon';
+import { Expense, ExpenseFormValues } from '@/types/expense';
+import { authGuard } from '@/lib/auth-utils';
 
-//TODO"
-export const createExpense = (expense: ExpenseFormValues) => {
-  console.log('Creating expense:', expense);
-};
+export const createExpense = authGuard(
+  session =>
+    async (expense: ExpenseFormValues): Promise<Expense> => {
+      const result = await sql`
+        INSERT INTO expense (
+          amount,
+          category_id,
+          date,
+          description,
+          user_id
+        ) VALUES (
+          ${expense.amount},
+          ${expense.categoryId},
+          ${expense.date},
+          ${expense.description},
+          ${session.user.id}
+        )
+        RETURNING
+          id,
+          amount,
+          category_id,
+          to_char(date, 'YYYY-MM-DD') AS date,
+          description,
+          created_at,
+          updated_at
+      `;
+
+      if (!result[0]) throw new Error('Failed to create expense');
+
+      // TODO: invalidate cache
+
+      return new Expense(result[0]);
+    },
+);
--- types/expense.ts
+++ types/expense.ts
@@ -1,6 +1,26 @@
 import { FormDateTime } from '@/lib/MuiDatePicker/utils';
 import { FormErrors } from './form';
 
+export class Expense {
+  id: number;
+  amount: number;
+  categoryId: number;
+  date: Date;
+  description: string;
+  createdAt: Date;
+  updatedAt: Date;
+
+  constructor(data: Record<string, any>) {
+    this.id = data.id;
+    this.amount = data.amount;
+    this.categoryId = data.category_id;
+    this.date = new Date(data.date);
+    this.description = data.description;
+    this.createdAt = new Date(data.created_at);
+    this.updatedAt = new Date(data.updated_at);
+  }
+}
+
 export interface ExpenseFormValues {
   amount: number | '';
   categoryId: number | '';
