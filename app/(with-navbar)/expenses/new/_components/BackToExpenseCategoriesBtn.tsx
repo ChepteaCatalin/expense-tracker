37a0be878ce2f9bdafa70ebcf4859e7b7ba4efa4
Catalin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2026-04-25 19:07:20 +0300

    navigate back link

--- app/(with-navbar)/expenses/new/_components/BackToExpenseCategoriesBtn.tsx
+++ app/(with-navbar)/expenses/new/_components/BackToExpenseCategoriesBtn.tsx
@@ -1,17 +1,19 @@
 'use client';
 
-import BackToLink from '@/components/BackToLink';
-import dayjs from 'dayjs';
+import Button from '@mui/material/Button';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import { useRouter } from 'next/navigation';
 
 export default function BackToExpenseCategoriesBtn() {
+  const router = useRouter();
+
   return (
-    <BackToLink
-      href={{
-        pathname: '/expenses/categories',
-        query: { day: dayjs().format('YYYY-MM-DD') },
-      }}
-      pageName="Expenses by Category"
-      sx={{ mb: 0.5 }}
-    />
+    <Button
+      sx={{ py: 0, px: 0.5, '& .MuiButton-startIcon': { mr: 0.5 }, mb: 0.5 }}
+      startIcon={<ChevronLeftIcon />}
+      onClick={() => router.back()}
+    >
+      Back
+    </Button>
   );
 }
