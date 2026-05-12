8befaee5d674f43cfd8ef16459c19fa1f3e40762
Catalin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2026-05-12 16:43:51 +0300

    remove flickering in the FAB btn

--- components/Fab.tsx
+++ components/Fab.tsx
@@ -1,20 +1,16 @@
-'use client';
-
 import MuiFab from '@mui/material/Fab';
 import AddIcon from '@mui/icons-material/Add';
-import useMediaQuery from '@mui/material/useMediaQuery';
 
 export default function Fab() {
-  const isDesktop = useMediaQuery('(pointer: fine)');
-
   return (
     <MuiFab
       color="primary"
       aria-label="add"
       sx={{
         position: 'fixed',
-        bottom: isDesktop ? 100 : 81,
+        bottom: 81,
         right: 10,
+        '@media (pointer: fine)': { bottom: 100 },
       }}
     >
       <AddIcon sx={{ fontSize: 28 }} />
