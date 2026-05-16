9a3ef3a30fe3f30a3251b8dfbe0bdb67af89b163
Catalin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2026-05-16 15:29:04 +0300

    remove BETTER_AUTH_URL

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,7 +5,7 @@ import { betterAuth } from 'better-auth';
 import { nextCookies } from 'better-auth/next-js';
 
 export const auth = betterAuth({
-  baseURL: process.env.BETTER_AUTH_URL,
+  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
   database: new Pool({
     connectionString: process.env.DATABASE_URL,
   }),
