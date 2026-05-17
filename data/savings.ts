5c714e2999b5be4616b260bb6f04d74af87126d1
Catalin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2026-05-17 16:20:13 +0300

    add DAL for getting savings goal by id

--- data/savings.ts
+++ data/savings.ts
@@ -3,6 +3,7 @@ import 'server-only';
 import { sql } from '@/lib/neon';
 import { authGuard } from '@/lib/auth-utils';
 import type { SavingsGoal, SavingsGoalFormValues } from '@/types/savings';
+import { cacheLife, cacheTag, updateTag } from 'next/cache';
 
 export const createSavingsGoal = authGuard(
   session =>
@@ -43,7 +44,8 @@ export const createSavingsGoal = authGuard(
 
       if (!created) throw new Error('Failed to create savings goal');
 
-      // TODO: invalidate
+      //TODO: invalidate list (add this cache key to it)
+      updateTag('savings-goals/list');
 
       return {
         id: created.id,
@@ -58,6 +60,55 @@ export const createSavingsGoal = authGuard(
           : undefined,
         notes: created.notes ?? undefined,
         currency: created.currency,
+        createdAt: new Date(created.created_at),
+        updatedAt: new Date(created.updated_at),
+      };
+    },
+);
+
+export const getSavingsGoalById = authGuard(
+  session =>
+    async (id: number): Promise<SavingsGoal | null> => {
+      'use cache';
+      cacheLife('weeks');
+      cacheTag(`savings-goals/id/${id}`);
+
+      const result = await sql`
+        SELECT
+          id,
+          name,
+          initial_amount,
+          target_amount,
+          to_char(start_date, 'YYYY-MM-DD') AS start_date,
+          is_completed,
+          to_char(completed_date, 'YYYY-MM-DD') AS completed_date,
+          notes,
+          currency,
+          created_at,
+          updated_at
+        FROM savings_goal
+        WHERE id = ${id}
+          AND user_id = ${session.user.id}
+      `;
+
+      const row = result[0];
+      if (!row) return null;
+
+      return {
+        id: row.id,
+        name: row.name,
+        initialAmount: row.initial_amount,
+        currentAmount: row.initial_amount, //TODO: real target amount, based on savings goals
+        targetAmount: row.target_amount,
+        startDate: new Date(row.start_date),
+        isCompleted: row.is_completed,
+        completedDate: row.completed_date
+          ? new Date(row.completed_date)
+          : undefined,
+        notes: row.notes ?? undefined,
+        currency: row.currency,
+        createdAt: new Date(row.created_at),
+        updatedAt: new Date(row.updated_at),
       };
     },
 );
