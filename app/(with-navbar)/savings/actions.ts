ee6f4446b277430fb1ed74ee1e43fce13da65310
Catalin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2026-05-18 18:39:33 +0300

    add updateSavingsGoal server action

--- app/(with-navbar)/savings/actions.ts
+++ app/(with-navbar)/savings/actions.ts
@@ -3,12 +3,16 @@
 import type {
   SavingsGoalFormErrors,
   SavingsGoalFormValues,
+  SavingsGoalFormValuesWithId,
 } from '@/types/savings';
 import { savingsGoalSchema } from './validation';
 import { getFormErrors } from '@/lib/zod';
 import { isUniqueViolationError, UnauthorizedError } from '@/utils/error';
 import { redirect } from 'next/navigation';
-import { createSavingsGoal as createNewSavingsGoal } from '@/data/savings';
+import {
+  createSavingsGoal as createNewSavingsGoal,
+  updateSavingsGoal as updateExistingSavingsGoal,
+} from '@/data/savings';
 import { toCents } from '@/utils/currency';
 
 export async function createSavingsGoal(
@@ -38,4 +42,33 @@ export async function createSavingsGoal(
   }
 
   redirect(`/savings`);
+}
+
+export async function updateSavingsGoal(
+  _: SavingsGoalFormErrors,
+  goal: SavingsGoalFormValuesWithId,
+): Promise<SavingsGoalFormErrors> {
+  const errors = getFormErrors(savingsGoalSchema, {
+    ...goal,
+    targetAmount: +goal.targetAmount,
+    initialAmount: +goal.initialAmount,
+    startDate: String(goal.startDate),
+  });
+  if (errors) return errors;
+
+  try {
+    await updateExistingSavingsGoal({
+      ...goal,
+      targetAmount: toCents(goal.targetAmount),
+      initialAmount: toCents(goal.initialAmount),
+    });
+  } catch (err: any) {
+    if (err instanceof UnauthorizedError) redirect('/signin');
+    if (isUniqueViolationError(err)) {
+      return { api: 'A goal with this name already exists' };
+    }
+    return { api: 'Failed to update the goal' };
+  }
+
+  redirect(`/savings/${goal.id}/details`);
 }
