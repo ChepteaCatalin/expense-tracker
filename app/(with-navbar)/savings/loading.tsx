288b0138900f3777374443c73e4ac981b1c13602
Catalin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2026-05-20 19:30:35 +0300

    loading fallback

--- app/(with-navbar)/savings/loading.tsx
+++ app/(with-navbar)/savings/loading.tsx
@@ -1,3 +1,31 @@
+import Heading from '@/components/Heading';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import { metadata } from './constants';
+import Skeleton from '@mui/material/Skeleton';
+
 export default function SavingsGoalsLoading() {
-  return <div>Loading...</div>;
+  return (
+    <Box sx={{ boxSizing: 'content-box', maxWidth: '610px', mx: 'auto' }}>
+      <Heading
+        title={metadata.title}
+        subtitle={metadata.description}
+        sx={{ mb: 5 }}
+      />
+      <Stack spacing={3}>
+        <SavingsGoalCard />
+        <SavingsGoalCard />
+      </Stack>
+    </Box>
+  );
+}
+
+function SavingsGoalCard() {
+  return (
+    <Skeleton
+      variant="rectangular"
+      height={331}
+      sx={{ borderRadius: '12px' }}
+    />
+  );
 }
