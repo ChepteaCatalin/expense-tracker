f6690ef6f6bc3159f9ed34d27c76b6bc071540af
Catalin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2026-05-22 22:07:55 +0300

    add shared layout

---
+++ app/(with-navbar)/savings/[id]/details/layout.tsx
@@ -0,0 +1,24 @@
+import PageWrapper from '@/components/PageWrapper';
+import Heading from '@/components/Heading';
+
+export const metadata = {
+  title: 'Goal Details',
+  description: 'Progress and details for your savings goal',
+};
+
+export default function SavingsGoalDetailsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <PageWrapper>
+      <Heading
+        title={metadata.title}
+        subtitle={metadata.description}
+        sx={{ mb: 5 }}
+      />
+      {children}
+    </PageWrapper>
+  );
+}
---
+++ app/(with-navbar)/savings/[id]/details/loading.tsx
@@ -0,0 +1,4 @@
+export default function SavingsGoalDetailsLoading() {
+  // TODO:
+  return <div>Loading...</div>;
+}
