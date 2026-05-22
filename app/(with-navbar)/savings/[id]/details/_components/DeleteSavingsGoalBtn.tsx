5aee1a097bc9d8701facd920f5717b5dfda38d4a
Catalin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2026-05-22 21:31:44 +0300

    implement delete savings goal

---
+++ app/(with-navbar)/savings/[id]/details/_components/DeleteSavingsGoalBtn.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import DeleteDialog from '@/components/DeleteDialog';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Button from '@mui/material/Button';
+import { useActionState, useState, startTransition } from 'react';
+import { deleteSavingsGoal } from '../../../actions';
+
+export default function DeleteSavingsGoalBtn({
+  id,
+  name,
+}: {
+  id: number;
+  name: string;
+}) {
+  const [open, setOpen] = useState(false);
+  const [hideError, setHideError] = useState(false);
+
+  const [actionError, deleteAction, isPending] = useActionState(
+    deleteSavingsGoal,
+    '',
+  );
+
+  return (
+    <>
+      <Button
+        variant="outlined"
+        color="error"
+        startIcon={<DeleteIcon />}
+        fullWidth
+        onClick={() => setOpen(true)}
+      >
+        Delete Goal
+      </Button>
+      <DeleteDialog
+        open={open}
+        type="savings goal"
+        name={name}
+        isPending={isPending}
+        error={actionError}
+        hideError={hideError}
+        handleClose={() => {
+          setOpen(false);
+          setHideError(true);
+        }}
+        handleDelete={() => {
+          setHideError(true);
+          startTransition(() => {
+            setHideError(false);
+            deleteAction(id);
+          });
+        }}
+      />
+    </>
+  );
+}
---
+++ app/(with-navbar)/savings/_components/BackToSavingsLink.tsx
@@ -0,0 +1,21 @@
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import Button from '@mui/material/Button';
+import Link from 'next/link';
+
+export function BackToSavingsLink() {
+  return (
+    <Link href="/savings">
+      <Button
+        sx={{
+          py: 0,
+          px: 0.5,
+          '& .MuiButton-startIcon': { mr: 0.5 },
+          mb: 0.5,
+        }}
+        startIcon={<ChevronLeftIcon />}
+      >
+        Savings
+      </Button>
+    </Link>
+  );
+}
