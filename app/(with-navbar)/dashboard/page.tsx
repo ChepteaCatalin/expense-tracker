302b35eb2faf7551ead41497f12b449d9436a3a0
Catalin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2026-06-03 13:16:53 +0300

    todos

--- app/(with-navbar)/dashboard/page.tsx
+++ app/(with-navbar)/dashboard/page.tsx
@@ -1,3 +1,10 @@
+// TODO: add metadata
+// TODO: add header
+
+// TODO: different layout for mobile/desktop cards
+// TODO: mobile navbar should fit all the items
+// TODO: loading fallback for mobile navbar
+
 export default function DashboardPage() {
   return <div>1</div>;
 }
