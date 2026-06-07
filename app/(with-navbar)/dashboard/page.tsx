6df847791b0a5326f708db0c8f61c80a7f92bdd0
Catalin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2026-06-07 13:40:40 +0300

    period changes

--- app/(with-navbar)/dashboard/page.tsx
+++ app/(with-navbar)/dashboard/page.tsx
@@ -1,6 +1,6 @@
 import Grid from '@mui/material/Grid';
 import InsightCard from './_components/InsightCard';
-import PeriodSelector from './_components/PeriodSelector';
+import Period from './_components/Period';
 import { notFound } from 'next/navigation';
 import type { DashboardSearchParams } from '@/types/dashboard';
 import { normalizedSearchParams, validSearchParams } from './utils';
@@ -18,7 +18,7 @@ export default async function DashboardPage({
 
   return (
     <Grid container spacing={2}>
-      <PeriodSelector />
+      <Period />
       <Grid size={{ xs: 12, md: 6 }}>
         <InsightCard title="Total Expenses">$1,250.00</InsightCard>
       </Grid>
