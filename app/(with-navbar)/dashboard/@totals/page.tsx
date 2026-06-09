4291cb7d0c82f10253f650f5f8d8ee6bbf5bd679
Catalin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2026-06-09 19:06:50 +0300

    loading fallback

--- app/(with-navbar)/dashboard/@totals/page.tsx
+++ app/(with-navbar)/dashboard/@totals/page.tsx
@@ -1,29 +1,24 @@
 import Typography from '@mui/material/Typography';
 import { getValidNormalizedSearchParams } from '../utils';
 import InsightCard from '../_components/InsightCard';
-import type { DashboardSearchParams, TotalsMetrics } from '@/types/dashboard';
+import type { DashboardSearchParams } from '@/types/dashboard';
 import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
 import { getSession } from '@/data/auth';
 import { readableCurrency } from '@/utils/currency';
-import { UnauthorizedError } from '@/utils/error';
 import { getDashboardTotals } from '@/data/dashboard';
-import { redirect } from 'next/navigation';
 
-export default async function Totals({
+export default async function TotalsPage({
   searchParams,
 }: {
   searchParams: Promise<DashboardSearchParams>;
 }) {
   const params = await getValidNormalizedSearchParams(searchParams);
 
-  let totals: TotalsMetrics;
-  try {
-    totals = await getDashboardTotals({ from: params.from!, to: params.to! });
-  } catch (err) {
-    if (err instanceof UnauthorizedError) redirect('/signin');
-    throw err;
-  }
+  const totals = await getDashboardTotals({
+    from: params.from!,
+    to: params.to!,
+  });
   const currency = (await getSession())?.user.currency;
 
   const netIncome = totals.income - totals.expenses;
