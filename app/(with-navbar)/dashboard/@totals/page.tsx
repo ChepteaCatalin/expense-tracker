a796756256466f1d7a9f19938466fb0e6d7de70d
Catalin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2026-06-09 18:12:32 +0300

    totals insight

--- app/(with-navbar)/dashboard/@totals/page.tsx
+++ app/(with-navbar)/dashboard/@totals/page.tsx
@@ -2,6 +2,9 @@ import Typography from '@mui/material/Typography';
 import { getValidNormalizedSearchParams } from '../utils';
 import InsightCard from '../_components/InsightCard';
 import type { DashboardSearchParams } from '@/types/dashboard';
+import Box from '@mui/material/Box';
+import { getSession } from '@/data/auth';
+import { readableCurrency } from '@/utils/currency';
 
 export default async function Totals({
   searchParams,
@@ -10,9 +13,32 @@ export default async function Totals({
 }) {
   const params = await getValidNormalizedSearchParams(searchParams);
 
+  //TODO: do this after fetching data
+  const currency = (await getSession())?.user.currency;
+
+  const totals = {
+    income: 1_234_56700.99,
+    expenses: 1_234_56700.99,
+    savings: 1_234_56700.99,
+  };
+
   return (
-    <InsightCard title="Totals">
-      <Typography sx={{ fontWeight: 500 }}>123</Typography>
+    <InsightCard title={`Totals (${currency})`}>
+      <MetricRow label="Total Income" value={totals.income} />
+      <MetricRow label="Total Expenses" value={totals.expenses} />
+      <MetricRow label="Net Income" value={totals.income - totals.expenses} />
+      <MetricRow label="Total Savings" value={totals.savings} />
     </InsightCard>
+  );
+}
+
+function MetricRow({ label, value }: { label: string; value: number }) {
+  return (
+    <Box>
+      <Typography component="span" sx={{ fontWeight: 600 }}>
+        {label}:{' '}
+      </Typography>
+      <Typography component="span">{readableCurrency(value)}</Typography>
+    </Box>
   );
 }
