d801f6c26aa8e0d90aee257ac2c555d6d76ef351
Catalin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2026-06-18 21:13:30 +0300

    add error fallback

---
+++ app/(with-navbar)/dashboard/@expensesBreakdown/error.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import InsightError from '../_components/InsightError';
+
+export default function ExpensesBreakdownError({
+  unstable_retry,
+}: {
+  unstable_retry: () => void;
+}) {
+  return <InsightError title="Expenses Breakdown" retry={unstable_retry} />;
+}
