71e9a1bcf319c9bcb5c4e6b8ea39a5078eceed1f
Catalin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2026-06-19 23:09:05 +0300

    proxy navigation

--- proxy.ts
+++ proxy.ts
@@ -1,10 +1,13 @@
-import dayjs from 'dayjs';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function proxy(request: NextRequest) {
   const url = new URL('/expenses/categories', request.url);
-  url.searchParams.set('month', dayjs().format('YYYY-MM-DD'));
+  const today = new Date();
+  const month = String(today.getMonth() + 1).padStart(2, '0');
+  const day = String(today.getDate()).padStart(2, '0');
+
+  url.searchParams.set('month', `${today.getFullYear()}-${month}-${day}`);
 
   return NextResponse.redirect(url);
 }
