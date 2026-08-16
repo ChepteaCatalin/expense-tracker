a8b19002c652c04f4df16329edf4e1062400247b
Cătălin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2026-08-16 16:07:01 +0300

    loading fallback

--- app/(with-navbar)/settings/@password/LoadingFallback.tsx
+++ app/(with-navbar)/settings/@password/LoadingFallback.tsx
@@ -1,34 +1,26 @@
-import Stack from "@mui/material/Stack";
-import Divider from "@mui/material/Divider";
-import Skeleton from "@mui/material/Skeleton";
 import Section from "../_components/Section";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function LoadingFallback() {
   return (
-    <Section title="Change Password">
-      <Stack spacing={3} sx={{ p: 1 }}>
-        <Skeleton
-          variant="rectangular"
-          height={40}
-          sx={{ borderRadius: "4px" }}
-        />
-        <Skeleton
-          variant="rectangular"
-          height={40}
-          sx={{ borderRadius: "4px" }}
-        />
-        <Skeleton
-          variant="rectangular"
-          height={40}
-          sx={{ borderRadius: "4px" }}
-        />
-        <Divider />
-        <Skeleton
-          variant="rectangular"
-          height={40}
-          sx={{ borderRadius: "4px" }}
-        />
-      </Stack>
+    <Section
+      title="Change Password"
+      footer={<Skeleton className="h-8 w-full" />}
+    >
+      <div className="grid gap-5 pb-px">
+        <div className="grid gap-2">
+          <Skeleton className="h-4.75 w-32.5" />
+          <Skeleton className="h-8" />
+        </div>
+        <div className="grid gap-2">
+          <Skeleton className="h-4.75 w-27.75" />
+          <Skeleton className="h-8" />
+        </div>
+        <div className="grid gap-2">
+          <Skeleton className="h-4.75 w-41.5" />
+          <Skeleton className="h-8" />
+        </div>
+      </div>
     </Section>
   );
 }
