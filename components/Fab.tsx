a11105c286c3f442eeed877e1badd327f841a82a
Cătălin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2026-09-19 21:50:59 +0300

    increase fab size for desktop

--- components/Fab.tsx
+++ components/Fab.tsx
@@ -5,7 +5,7 @@ export default function Fab() {
   return (
     <Button
       size="icon-lg"
-      className="fixed right-3 bottom-17.25 h-12 w-12 rounded-full lg:bottom-23.75"
+      className="fixed right-3 bottom-17.25 h-12 w-12 rounded-full lg:bottom-23.75 lg:h-14 lg:w-14"
       aria-label="add"
     >
       <Plus />
