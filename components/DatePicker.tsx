3aa27f0136e5914b9dab886c1789fce45b8756fe
Cătălin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2026-09-26 16:11:39 +0300

    fix datepicker border

--- components/DatePicker.tsx
+++ components/DatePicker.tsx
@@ -30,7 +30,7 @@ export default function DatePicker({
         render={
           <Button
             variant="outline"
-            className="justify-start font-normal disabled:bg-[color-mix(in_oklab,var(--input)_80%,transparent)]!"
+            className="disabled:bg-input/50 dark:disabled:bg-input/80 justify-start font-normal"
             disabled={disabled}
           >
             {value ? format(date!, "d MMMM yyyy") : <span>Pick a date</span>}
